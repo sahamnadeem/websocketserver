13a725eb36235582b95cb5deacb249dcbe614c6c
sahamnadeem <sahamnadeem2001@outlook.com>
2019-11-15 10:45:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('http').createServer()
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+var users = [];
+
+io.on('connection', function (socket) {
+  socket.on('user_id',(data)=>{
+    users.push({
+      user_id:data.id,
+      socket:socket
+    });
+    console.log(users.length)
+  })
+  socket.on('disconnect',(data)=>{
+    let index = users.indexOf(socket)
+    users.splice(1,index+1)
+    console.log(users)
+  })
+  socket.emit('message', { my: 'you are successfully connected' });
+});
